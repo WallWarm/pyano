7434117c0a3f505be7a4a6b2d15b48f177c2658c
guntrr <arnavguntur.2009@gmail.com>
2025-11-07 14:21:06 +0530

    added maxtime of 120ms

--- sound.py
+++ sound.py
@@ -9,9 +9,9 @@ def play():
         keys = pygame.key.get_pressed()
         if keys[x]:
             note = pygame.mixer.Sound(n.whitenotes[d.wbkeys.index(x)]+'.mp3')
-            note.play()
+            note.play(maxtime=120)
     for y in d.dbbkeys.keys():
         keys = pygame.key.get_pressed()
         if keys[y]:
             note = pygame.mixer.Sound(n.blacknotes[d.bbkeys.index(y)]+'.mp3')
-            note.play()
+            note.play(maxtime=120)
